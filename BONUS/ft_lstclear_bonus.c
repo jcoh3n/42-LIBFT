32503d5d7c517aeaea894ba311cceae18ad3c2de
Joey Cohen <jcohen@paul-f4Br6s8.clusters.42paris.fr>
2024-05-24 20:55:34 +0200

    All BONUS done

---
+++ BONUS/ft_lstclear_bonus.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 19:46:23 by jcohen            #+#    #+#             */
+/*   Updated: 2024/05/24 20:26:15 by jcohen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+	t_list	*next;
+
+	if (!*lst || !del)
+		return ;
+	current = *lst;
+	while (current != NULL)
+	{
+		next = current->next;
+		del(current->content);
+		free(current);
+		current = next;
+	}
+	*lst = NULL;
+}
---
+++ BONUS/ft_lstmap_bonus.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 20:32:21 by jcohen            #+#    #+#             */
+/*   Updated: 2024/05/24 20:51:14 by jcohen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static t_list	*create_new_elem(void *content)
+{
+	t_list	*new_elem;
+
+	new_elem = ft_lstnew(content);
+	if (!new_elem)
+		return (NULL);
+	return (new_elem);
+}
+
+static void	free_map(t_list *map, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	while (map)
+	{
+		tmp = map->next;
+		ft_lstdelone(map, del);
+		map = tmp;
+	}
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*map;
+	t_list	*new_elem;
+	void	*transformed_content;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	map = NULL;
+	while (lst)
+	{
+		transformed_content = f(lst->content);
+		new_elem = create_new_elem(transformed_content);
+		if (!new_elem)
+		{
+			free_map(map, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&map, new_elem);
+		lst = lst->next;
+	}
+	return (map);
+}
