f9399d28015c139a2c6640b0907b25d96b60557d
jcohen <jcohen@student.42.fr>
2024-10-18 18:19:30 +0200

    update ft_strdup()

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 20:21:05 by jcohen            #+#    #+#             */
-/*   Updated: 2024/05/22 12:43:13 by jcohen           ###   ########.fr       */
+/*   Updated: 2024/10/18 18:14:32 by jcohen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,15 @@
 
 char	*ft_strdup(const char *s)
 {
+	size_t	len;
 	char	*duplicate;
-	int		len_s;
 
-	len_s = 0;
-	while (s[len_s])
-	{
-		len_s++;
-	}
-	duplicate = (char *)malloc(sizeof(char) * len_s + 1);
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	duplicate = (char *)malloc(sizeof(char) * (len + 1));
 	if (!duplicate)
 		return (NULL);
-	len_s = 0;
-	while (s[len_s])
-	{
-		duplicate[len_s] = s[len_s];
-		len_s++;
-	}
-	duplicate[len_s] = '\0';
+	ft_memcpy(duplicate, s, len + 1);
 	return (duplicate);
 }
