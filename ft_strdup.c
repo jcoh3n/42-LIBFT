e14d4e459d7bca4381074c2dc6585afbc1a11853
g-garibotti <genarogaribotti@hotmail.fr>
2024-12-17 21:27:45 +0100

    new strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ggaribot <ggaribot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 20:21:05 by jcohen            #+#    #+#             */
-/*   Updated: 2024/10/18 18:14:32 by jcohen           ###   ########.fr       */
+/*   Updated: 2024/12/17 21:15:39 by ggaribot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strdup(const char *s)
 	char	*duplicate;
 
 	if (!s)
-		return (NULL);
+		return (ft_strdup(""));
 	len = ft_strlen(s);
 	duplicate = (char *)malloc(sizeof(char) * (len + 1));
 	if (!duplicate)
